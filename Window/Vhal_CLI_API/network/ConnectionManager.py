fa87a8c13adea189fc27ca94d951a5af77935841
jyjang <jyjang@suresofttech.com>
2024-06-03 15:59:46 +0900

    feat : 로컬, 원격 연결 관리 클래스 구현

--- Window/Vhal_CLI_API/network/ConnectionManager.py
+++ Window/Vhal_CLI_API/network/ConnectionManager.py
@@ -1,21 +1,30 @@
 import logging
 import subprocess
+from typing import Optional
+from enum import Enum
+
+
+class ConnectionType(Enum):
+    LOCAL = "local"
+    REMOTE = "remote"
+
+
 import typer
 
-import local_connection_model
-import remote_connection_model
+# local_connection_model.py와 remote_connection_model.py에서 클래스 가져오기
+from local_connection_model import local_connection_model
+from remote_connection_model import remote_connection_model
 
-app = typer.Typer()
 
 
 class ConnectionManager:
     _instance = None
     process = None
-    app = typer.Typer()
 
     def __init__(self):
-        self.local_connection_model = None
-        self.remote_connection_model = None
+        self.ConnectionType: ConnectionType
+        self.local_connection_model: local_connection_model = local_connection_model()
+        self.remote_connection_model: remote_connection_model = remote_connection_model()
 
     def __new__(cls):
         if cls._instance is None:
@@ -28,47 +37,62 @@ class ConnectionManager:
                                         stderr=subprocess.PIPE,
                                         text=True)
 
-    @app.command()
-    def set_connection(self, connection_type: str, address: str, port: int):
+    def set_connection(self, connection_type: str, address: Optional[str], port: Optional[str]):
         if connection_type == "l" or connection_type == "local":
-            self.set_local_connection()
+            self.set_local_connection(address, port)
         elif connection_type == "r" or connection_type == "remote":
             self.set_remote_connection(address, port)
         else:
             logging.error("Error: Please enter a valid command")
 
-    @app.command()
+    def change(self):
+        type = self.get_connection_type()
+        if type == "local":
+            self.connection_remote()
+        elif type == "remote":
+            self.connection_local()
+        else:
+            typer.echo("ADB와 애뮬레이터 연결이 안되어있어 전환을 할 수 없습니다.")
+
+    def connection_remote(self):
+        if self.remote_connection_model.get_port() is None or self.remote_connection_model.get_address() is None:
+            typer.echo("Error : Remote connection information is Not True")
+            return
+        command = self.remote_connection_model.get_address() + " : " + self.remote_connection_model.get_port()
+        self.ConnectionType = "remote"
+        self.run_command(command)
+
+    def connection_local(self):
+        if self.local_connection_model.get_port() is None or self.local_connection_model.get_address() is None:
+            typer.echo("Error : Local connection information is Not True")
+            return
+        command = self.local_connection_model.get_address() + " : " + self.local_connection_model.get_port()
+        self.ConnectionType = "local"
+        self.run_command(command)
+
     def connection(self, connection_type: str):
         if connection_type == "l" or connection_type == "local":
-            if self.local_connection_model.get_port() is None or self.local_connection_model.get_address() is None:
-                typer.echo("Error : Local connection information is Not True")
-                return
-            command = self.local_connection_model.get_address() + " : " + self.local_connection_model.get_port()
-            self.run_command(command)
+            self.connection_local()
         elif connection_type == "r" or connection_type == "remote":
-            if self.remote_connection_model.get_port() is None or self.remote_connection_model.get_address() is None:
-                typer.echo("Error : Remote connection information is Not True")
-                return
-            command = self.remote_connection_model.get_address() + " : " + self.remote_connection_model.get_port()
-            self.run_command(command)
+            self.connection_remote()
 
-    def set_local_connection(self, ip: None, port: None):
-        if ip is None:
+    def set_local_connection(self, address: Optional[str], port: Optional[str]):
+        if address is None:
             self.local_connection_model.set_address("10.10.10.10")
             typer.echo("Success : Setting local IP : 10.10.10.10")
         else:
-            self.local_connection_model.set_address(ip)
-            typer.echo(f"Success : Setting local IP : {ip}")
+            self.local_connection_model.set_address(address)
+            typer.echo(f"Success : Setting local IP : {address}")
 
         if port is None:
-            self.local_connection_model.set_port(8888)
+            self.local_connection_model.set_port("8888")
             typer.echo("Success : Setting local port : 8888")
         else:
             self.local_connection_model.set_port(port)
             typer.echo(f"Success : Setting local port : {port}")
 
-    def set_remote_connection(self, ip: str, port: int):
-        self.remote_connection_model.set_address(ip)
+    def set_remote_connection(self, address: Optional[str], port: Optional[str]):
+        self.remote_connection_model.set_address(address)
         self.remote_connection_model.set_port(port)
 
     def run_command(self, command: str):
@@ -78,6 +102,9 @@ class ConnectionManager:
             self.process.stdin.flush()
         except ConnectionError:
             typer.echo("Error : Can't Connect ")
+
+    def get_connection_type(self):
+        return self.ConnectionType.value
 
 
 connection_instance = ConnectionManager()
